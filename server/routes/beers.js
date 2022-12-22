85eb31cb28322d9129dcfbc36fcd3bada558759d
glrose <garrett.rose03@gmail.com>
2022-12-22 20:07:56 +0200

    Express router for mongoose post request

--- server/routes/beers.js
+++ server/routes/beers.js
@@ -1,1 +1,14 @@
-//store routes for mongoose CRUD
+const express = require("express");
+const Beer = require("../models/beerSchema");
+const router = express.Router();
+
+router.route("/new").post((req, res) => {
+  const newBeer = new Beer(req.body);
+
+  newBeer
+    .save()
+    .then((beer) => res.json(beer))
+    .catch((err) => res.status(400).json("Error! " + err));
+});
+
+module.exports = router;
